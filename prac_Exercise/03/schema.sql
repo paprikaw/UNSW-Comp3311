4c3bd116797de12d6cff2c5aed6959eb463d99bb
Eric Bai <baixu.must@gmail.com>
2021-03-07 08:17:07 +0800

    Lab03: Add constraint to schema

--- prac_Exercise/03/schema.sql
+++ prac_Exercise/03/schema.sql
@@ -2,25 +2,32 @@
 -- Schema for simple company database
 
 create table Employees (
-	tfn         char(11),
-	givenName   varchar(30),
+	tfn         char(11) where tfn ~ '[0-9]{3}-[0-9]{3}-[0-9]{3}',
+	givenName   varchar(30) NOT NULL,
 	familyName  varchar(30),
-	hoursPweek  float
+	hoursPweek  float check  (hourspweek <= 168 and hourspweek >= 0) 
+	primary key (tfn)
 );
 
 create table Departments (
-	id          char(3),
-	name        varchar(100),
-	manager     char(11)
+	id          char(3) UNIQUE,
+	name        varchar(100) UNIQUE,
+	manager     char(11),
+	primary key (id),
 );
 
 create table DeptMissions (
 	department  char(3),
-	keyword     varchar(20)
+	keyword     varchar(20),
+	primary key (department, keyword),
+	foreign key (department) REFERENCES departments(id)
 );
 
 create table WorksFor (
 	employee    char(11),
 	department  char(3),
-	percentage  float
+	percentage  float check (percentage > 0 and percentage <= 1),
+	primary key (employee,department),
+	foreign key (employee) REFERENCES employees(tfn),
+	foreign key (department) REFERENCES departments(id)
 );
